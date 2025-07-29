2f7a699e77ab28d9148278384b6aa9a243e9195e
Maxime Britto <maxime.britto@unify-technologies.com>
2025-07-29 14:16:44 +0200

    Renommage du dossier templates en docs et mise à jour des chemins

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify, send_from_directory
 import os
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='docs')
 
 @app.route('/')
 def home():
